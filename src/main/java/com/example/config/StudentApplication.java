377074844b9bd31ef9b088a049a11a3040703ff2
abc <vaibhavi.warke79@gmail.com>
2019-11-02 12:09:15 +0530

    Added new Files

---
+++ src/main/java/com/example/config/StudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+			SpringApplication.run(StudentApplication.class, args);
+}
